e908a8fac2b07eb64c61a049b081eeaf889cd974
yama9312 <yama9312@pacificu.edu>
2024-10-14 17:36:42 -0700

    working on start page

--- app/src/main/java/com/example/vetappdraft/MainActivity.java
+++ app/src/main/java/com/example/vetappdraft/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.vetappdraft;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner mSpinChoice;
+    private String sBranch;
+    private Button btnSubmit;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnSubmit = findViewById(R.id.btnSubBranch);
+
+        btnSubmit.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sBranch = mSpinChoice.getSelectedItem().toString();
+                    }
+                }
+        );
+
+
+        String[] choiceArray = new String[] {
+                "Army", "Marine Corps", "Navy", "Air Force"
+        };
+        ArrayAdapter<String> choiceAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_spinner_dropdown_item, choiceArray);
+        choiceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinChoice = (Spinner) findViewById(R.id.spnBranch);
+        mSpinChoice.setAdapter(choiceAdapter);
     }
 }
