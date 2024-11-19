8dd08ab0dbae39978ced36b853a6b2e4de03fc81
yama9312 <Yama9312@pacificu.edu>
2024-11-18 18:01:19 -0800

    working on step adapter.

--- app/src/main/java/com/example/vetappdraft/ChangeStepOrder.java
+++ app/src/main/java/com/example/vetappdraft/ChangeStepOrder.java
@@ -12,9 +12,15 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class ChangeStepOrder extends BaseActivity {
-    public StepAdapter theSteps;
+    public ArrayList<Page> theSteps;
     private RecyclerView recyclerView;
+
+    private StepAdapter mcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +32,22 @@ public class ChangeStepOrder extends BaseActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        theSteps = new StepAdapter();
-        theSteps.addPage(new Page("Step1", step1.class, "no", "take a deep breath"));
-        theSteps.addPage(new Page("Step2", step1.class, "no", "Review reasons for living"));
-        theSteps.addPage(new Page("Step3", step1.class, "no", "H.A.L.T"));
-        theSteps.addPage(new Page("Step4", step1.class, "no", "Practice Breathing Skills"));
-        theSteps.addPage(new Page("Step5", step1.class, "no", "Identify current thoughts, emotions, and body sensations"));
-        theSteps.addPage(new Page("Step6", step1.class, "no", "Do Body Scan Meditation"));
-        theSteps.addPage(new Page("Step7", step1.class, "no", "Listen to Music"));
-        theSteps.addPage(new Page("Step8", step1.class, "no", "Call Someone / See who is online"));
-        theSteps.addPage(new Page("Step9", step1.class, "no", "Call Hotlines"));
-        theSteps.addPage(new Page("Step10", step1.class, "no", "Put down the weapon and put both hands on the phone"));
+        theSteps = new ArrayList<Page> ();
+        theSteps.add(new Page("Step1", step1.class, "no", "take a deep breath"));
+        theSteps.add(new Page("Step2", step1.class, "no", "Review reasons for living"));
+        theSteps.add(new Page("Step3", step1.class, "no", "H.A.L.T"));
+        theSteps.add(new Page("Step4", step1.class, "no", "Practice Breathing Skills"));
+        theSteps.add(new Page("Step5", step1.class, "no", "Identify current thoughts, emotions, and body sensations"));
+        theSteps.add(new Page("Step6", step1.class, "no", "Do Body Scan Meditation"));
+        theSteps.add(new Page("Step7", step1.class, "no", "Listen to Music"));
+        theSteps.add(new Page("Step8", step1.class, "no", "Call Someone / See who is online"));
+        theSteps.add(new Page("Step9", step1.class, "no", "Call Hotlines"));
+        theSteps.add(new Page("Step10", step1.class, "no", "Put down the weapon and put both hands on the phone"));
 
         RecyclerView rvOrder = findViewById(R.id.rvStepOrder);
+        rvOrder.setHasFixedSize(true);
+        rvOrder.setLayoutManager(new LinearLayoutManager (this));
+
 
 
     }
--- app/src/main/java/com/example/vetappdraft/StepAdapter.java
+++ app/src/main/java/com/example/vetappdraft/StepAdapter.java
@@ -1,6 +1,5 @@
 package com.example.vetappdraft;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,55 +8,62 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import java.util.ArrayList;
 
-public class StepAdapter extends RecyclerView.Adapter<StepAdapter.ViewHolder> {
-    private ArrayList<Page> steps;
-    private Context context;
-
-    public StepAdapter(ArrayList<Page> pages, Context context) {
-        this.steps = pages;
-        this.context = context;
-    }
-    public StepAdapter () {
-        this.steps = new ArrayList<>();
-    }
-
-    public void addPage(Page insert) {
-        steps.add(insert);
-        notifyItemInserted(steps.size() - 1);
+public class StepAdapter extends
+    RecyclerView.Adapter<StepAdapter.ViewHolder>
+{
+    private ArrayList<Page> mcData;
+    /***
+     * Constructor to build the adapter
+     * @param data the data to be displayed by the views
+     */
+    public StepAdapter(ArrayList<Page> data) {
+        this.mcData = data;
     }
 
-    public void onItemMove(int fromPosition, int toPosition) {
-        Page movedPage = steps.remove(fromPosition);
-        steps.add(toPosition, movedPage);
-        notifyItemMoved(fromPosition, toPosition);
-    }
-
+    // create the view from the xml file by inflating
     @NonNull
     @Override
-    public StepAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_string, parent, false);
-        return new ViewHolder(view);
+    public StepAdapter.ViewHolder onCreateViewHolder (
+        @NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(parent.getContext())
+            .inflate(R.layout.display_step, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    // tells us what position in the array list we need to display and binds data to screen
+    @Override
+    public void onBindViewHolder (
+        @NonNull StepAdapter.ViewHolder holder, int position)
+    {
+        holder.mcCourse = mcData.get(position);
+        holder.bindData();
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StepAdapter.ViewHolder holder, int position) {
-        Page page = steps.get(position);
-        holder.titleTextView.setText(page.getName());
+    public int getItemCount ()
+    {
+        return mcData.size();
     }
 
-    @Override
-    public int getItemCount() {
-        return steps.size();
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView titleTextView;
-
+    // represents the piece of xml on the screen and puts course data in each widget
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        private Page mcCourse;
+        private TextView mcTVPageInfo;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            titleTextView = itemView.findViewById(R.id.textView);
+        }
+        public void bindData() {
+            if (null == mcTVPageInfo) {
+                mcTVPageInfo = (TextView) itemView.findViewById(R.id.tvStep);
+            }
+            mcTVPageInfo.setText(mcCourse.getInstructions ());
         }
     }
 }
+
