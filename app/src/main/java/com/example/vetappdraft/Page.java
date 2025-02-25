3c02585f036c54a926e40c4782f94e0cac4d80d2
yama9312 <yama9312@pacificu.edu>
2025-02-25 15:46:27 -0800

    added page objects

--- app/src/main/java/com/example/vetappdraft/Page.java
+++ app/src/main/java/com/example/vetappdraft/Page.java
@@ -6,6 +6,9 @@ public class Page {
     private final String mcLink;
     private final String mcInstructions;
 
+    // need to change class to fragment
+    // after change need to add another fragment option that will route user to second linked step
+    // when all items are made vector of Pages will be sorted and stored in the DB
     public Page(String name, Class<?> activityClass, String mcLink, String mcInstructions) {
         this.mcName = name;
         this.mcActivityClass = activityClass;
