60241a80b6d5b5fc1142ec9a03873c1ad7cda6c4
yama9312 <yama9312@pacificu.edu>
2025-04-22 15:12:18 -0700

    added routes to audio files to page objects

--- app/src/main/java/com/example/vetappdraft/MainActivity.java
+++ app/src/main/java/com/example/vetappdraft/MainActivity.java
@@ -1,40 +1,12 @@
 package com.example.vetappdraft;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.drawable.GradientDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import java.net.URLEncoder;
-import java.util.Vector;
-import java.util.concurrent.Executors;
 import java.util.LinkedList;
-
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 public class MainActivity extends BaseActivity {
 
@@ -42,9 +14,10 @@ public class MainActivity extends BaseActivity {
 
     /**
      * Retrieves the list of pages in the app.
+     *
      * @return A vector containing the pages.
      */
-    public Vector<Page> getPages(){
+    public LinkedList<Page> getPages(){
         return pages;
     }
 
@@ -61,31 +34,31 @@ public class MainActivity extends BaseActivity {
         pages.add(new Page("step 1", Page.PageType.TEXT, "take a deep breath", ""));
         pages.add(new Page("step 2", Page.PageType.TEXT, "review reasons for living\nPhone a friend", ""));
         pages.add(new Page("step 3", Page.PageType.TEXT, "H.A.L.T", ""));
-        pages.add(new Page("step 4", Page.PageType.TEXT, "2 minute mindful breathing", ""));
+        pages.add(new Page("step 4", Page.PageType.TEXT, "2 minute mindful breathing", "", R.raw.Two_Minutes_of_Mindful_Breathing_VF));
         pages.add(new Page("step 5", Page.PageType.TEXT, "safetySOP", ""));
-        pages.add(new Page("step 6", Page.PageType.TEXT, "body scan meditation", ""));
+        pages.add(new Page("step 6", Page.PageType.TEXT, "body scan meditation", "", R.raw.Body_Scan_v2));
         pages.add(new Page("step 7", Page.PageType.TEXT, "listen to music", ""));
         pages.add(new Page("step 8", Page.PageType.TEXT, "call someone\nsee who is online", ""));
         pages.add(new Page("step 9", Page.PageType.TEXT, "call hotlines", ""));
         pages.add(new Page("step 10", Page.PageType.TEXT, "put weapons down", ""));
-        pages.add(new Page("step 11", Page.PageType.TEXT, "observe & describe", ""));
-        pages.add(new Page("step 12", Page.PageType.TEXT, "leaves on a stream", ""));
+        pages.add(new Page("step 11", Page.PageType.TEXT, "observe & describe", "", R.raw.Observe_and_Describe_VF));
+        pages.add(new Page("step 12", Page.PageType.TEXT, "leaves on a stream", "", R.raw.Leaves_on_a_Stream_VF));
         pages.add(new Page("step 13", Page.PageType.TEXT, "identify values", ""));
-        pages.add(new Page("step 14", Page.PageType.TEXT, "mindful walking", ""));
-        pages.add(new Page("step 15", Page.PageType.TEXT, "progressive muscle\nrelaxation", ""));
+        pages.add(new Page("step 14", Page.PageType.TEXT, "mindful walking", "", R.raw.Mindful_Walking_VF));
+        pages.add(new Page("step 15", Page.PageType.TEXT, "progressive muscle\nrelaxation", "", R.raw.Progressive_Muscle_Relaxing_VF));
         pages.add(new Page("step 16", Page.PageType.TEXT, "distract yourself", ""));
-        pages.add(new Page("step 17", Page.PageType.TEXT, "awareness of thought", ""));
+        pages.add(new Page("step 17", Page.PageType.TEXT, "awareness of thought", "", R.raw.Awareness_of_Thoughts_VF));
         pages.add(new Page("step 18", Page.PageType.TEXT, "follow the circle", ""));
-        pages.add(new Page("step 19", Page.PageType.TEXT, "urge surfing", ""));
+        pages.add(new Page("step 19", Page.PageType.TEXT, "urge surfing", "", R.raw.Urge_Surfing_VF));
         pages.add(new Page("step 20", Page.PageType.TEXT, "SOBER\n-Stop\n-observe\n-breath\n-expand\n-respond", ""));
-        pages.add(new Page("step 21", Page.PageType.TEXT, "four squared breathing", ""));
-        pages.add(new Page("step 22", Page.PageType.TEXT, "diaphragmatic breathing", ""));
-        pages.add(new Page("step 23", Page.PageType.TEXT, "5 senses", ""));
-        pages.add(new Page("step 24", Page.PageType.TEXT, "mountain meditation", ""));
+        pages.add(new Page("step 21", Page.PageType.TEXT, "four squared breathing", "", R.raw.Four_Square_Breathing_VF));
+        pages.add(new Page("step 22", Page.PageType.TEXT, "diaphragmatic breathing", "", R.raw.Diagramic_Breathing_VF));
+        pages.add(new Page("step 23", Page.PageType.TEXT, "5 senses", "", R.raw.Five_Senses_VF));
+        pages.add(new Page("step 24", Page.PageType.TEXT, "mountain meditation", "", R.raw.Mountian_Meditation_VF));
         pages.add(new Page("step 25", Page.PageType.TEXT, "distractions", ""));
-        pages.add(new Page("step 26", Page.PageType.TEXT, "whole body breathing", ""));
-        pages.add(new Page("step 27", Page.PageType.TEXT, "breathing space", ""));
-        pages.add(new Page("step 28", Page.PageType.TEXT, "breathing body", ""));
+        pages.add(new Page("step 26", Page.PageType.TEXT, "whole body breathing", "", R.raw.Whole_Body_Breathing_Meditation_VF));
+        pages.add(new Page("step 27", Page.PageType.TEXT, "breathing space", "", R.raw.Breathing_Space_VF));
+        pages.add(new Page("step 28", Page.PageType.TEXT, "breathing body", "", R.raw.Breathing_Body_VF));
 
         // Add NavigationBarFragment
         FragmentManager fragmentManager = getSupportFragmentManager();
--- app/src/main/java/com/example/vetappdraft/Page.java
+++ app/src/main/java/com/example/vetappdraft/Page.java
@@ -10,16 +10,25 @@ public class Page {
     private final PageType mcType;
     private final String mcContent;
     private final String mcInstructions;
+    private final int audioResID;
 
     // need to change class to fragment
     // after change need to add another fragment option that will route user to second linked step
     // when all items are made vector of Pages will be sorted and stored in the DB
 
+    public Page(String name, PageType type, String content, String instructions, int audioResID) {
+        this.mcName = name;
+        this.mcType = type;
+        this.mcContent = content;
+        this.mcInstructions = instructions;
+        this.audioResID = audioResID;
+    }
     public Page(String name, PageType type, String content, String instructions) {
         this.mcName = name;
         this.mcType = type;
         this.mcContent = content;
         this.mcInstructions = instructions;
+        this.audioResID = -999;
     }
 
     public String getName() {
@@ -37,5 +46,6 @@ public class Page {
     public String getInstructions() {
         return mcInstructions;
     }
+    public int getAudioResId() {return audioResID;}
 }
 
