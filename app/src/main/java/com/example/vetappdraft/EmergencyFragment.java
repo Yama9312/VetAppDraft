60cd1b0f627c2ab6aefda529213e105d0489caa2
yama9312 <Yama9312@pacificu.edu>
2025-06-04 12:49:15 -0700

    page crashes as soon as I try to access emergency contact page

--- app/src/main/java/com/example/vetappdraft/EmergencyFragment.java
+++ app/src/main/java/com/example/vetappdraft/EmergencyFragment.java
@@ -1,5 +1,7 @@
 package com.example.vetappdraft;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +11,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * EmergencyFragment displays emergency contacts like 911 etc
@@ -16,9 +21,8 @@ import android.view.ViewGroup;
  * there's more to add to this fragments - mostly just textviews
  * @author ania
  */
+public class EmergencyFragment extends Fragment {
 
-public class EmergencyFragment extends Fragment
-{
   /**
    * Inflates the layout for the EmergencyFragment.
    *
@@ -30,8 +34,7 @@ public class EmergencyFragment extends Fragment
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_emergency, container, false);
-    return view;
+    return inflater.inflate(R.layout.fragment_emergency, container, false);
   }
 
   /**
@@ -43,5 +46,24 @@ public class EmergencyFragment extends Fragment
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+
+    VetDatabase db = VetDatabase.getInstance(requireContext());
+    VetDAO dao = db.vetDAO();
+
+    List<VetUser> contacts = dao.getAll();
+    TextView emergencyTextView = view.findViewById(R.id.textViewFriend);
+
+    if (contacts != null && !contacts.isEmpty()) {
+      String emergencyNumber = contacts.get(0).getMcEContact();
+      emergencyTextView.setText(emergencyNumber);
+      emergencyTextView.setOnClickListener(v -> {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + emergencyNumber));
+        startActivity(intent);
+      });
+    } else {
+      emergencyTextView.setText("No emergency contact found");
+      emergencyTextView.setOnClickListener(null);
+    }
   }
 }
